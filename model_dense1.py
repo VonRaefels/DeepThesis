ba0740117ac9036c6a955c6ecaca0d6fc58a3eef
VonRaefels <jorgemadridportillo@gmail.com>
2017-03-15 15:13:07 +0100

    reinitializing files

---
+++ model_dense1.py
@@ -0,0 +1,38 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
+import numpy as np
+np.random.seed(123)
+from keras.utils import np_utils
+
+from keras.optimizers import SGD
+
+
+def load_data():
+    print('loading data...')
+
+
+def create_model():
+    print('-----creating model-----')
+    model = Sequential()
+    model.add(Dense(1024, batch_input_shape=(None, 154), activation='relu'))
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(154, activation='sigmoid'))
+
+
+    sgd = SGD(lr=0.003, decay=0.0)
+    model.compile(loss='mean_squared_error',
+                  optimizer='sgd',
+                  metrics=['accuracy'])
+
+def train_and_evaluate_model():
+    print('training and evaluation')
+    model.fit(X_train, Y_train, batch_size=199, nb_epoch=2000, verbose=1)
+
+
+if __name__ == "__main__":
+    print('Starting...')
+    data, targets = load_data()
+    #n_folds = 1
+    #skf = StratifiedKFold(labels, n_folds=n_folds, shuffle=True)
---
+++ synthesizer.m
@@ -0,0 +1,70 @@
+mic1 = 1;
+mic2 = 2;
+FS = 16000;
+S = FS*2;
+N = 1000;
+load 'Impulse_response_Acoustic_Lab_Bar-Ilan_University_(Reverberation_0.160s)_3-3-3-8-3-3-3/Impulse_response_Acoustic_Lab_Bar-Ilan_University_(Reverberation_0.160s)_3-3-3-8-3-3-3_1m_000.mat';
+h0 = impulse_response; 
+load 'Impulse_response_Acoustic_Lab_Bar-Ilan_University_(Reverberation_0.160s)_3-3-3-8-3-3-3/Impulse_response_Acoustic_Lab_Bar-Ilan_University_(Reverberation_0.160s)_3-3-3-8-3-3-3_1m_030.mat';
+h30 = impulse_response;
+factor = 0.35;
+parent_dir = 'data_timit/TRAIN';
+d = rdir('data_timit/TRAIN/**/*.WAV');
+speech = zeros(numel(d), S);
+for k=1:numel(d)
+    [y Fs] = audioread(d(k).name);
+    l = size(y, 1);
+    if l > S
+        l = S;
+    end
+    speech(k,1:l) = y(1:l)';
+end
+disp('writing data')
+for i = 1:N
+    j = randi([1 numel(d)]);
+    l = randi([1 numel(d)]);
+    dir_i = d(j).folder;
+    dir_j = d(l).folder;
+    while strcmp(dir_i, dir_j) == true
+        l = randi([1 numel(d)]);
+        dir_j = d(l).folder;
+        disp('same dir');
+        disp(dir_i);
+        disp(dir_j);
+    end
+
+    target = speech(j,:);
+    echo = factor*speech(l,:);
+    %target = wgn(FS, 1, 0);
+    %echo = factor*wgn(FS, 1, 0);
+    %soundsc(n, FS);
+
+    y11 = conv(target, h0(:,mic1));
+    y12 = conv(target, h0(:,mic2));
+
+    y21 = conv(echo, h30(:,mic1));
+    y22 = conv(echo, h30(:,mic2));
+
+    out1 = y11 + y21;
+    out2 = y12 + y22;
+
+%     out1 = out1 / max(abs(out1));
+%     out2 = out2 / max(abs(out2));
+%     target = target / max(abs(target));
+%     echo = echo / max(abs(echo));
+%     y11 = y11 / max(abs(y11));
+%     y22 = y22 / max(abs(y22));
+%     y12 = y12 / max(abs(y12));
+%     y21 = y21 / max(abs(y21));
+
+    mkdir(strcat('/home/jorge/Documents/data/', num2str(i)));
+    audiowrite(sprintf('/home/jorge/Documents/data/%d/mix1.wav', i), out1(1:S), FS);
+    audiowrite(sprintf('/home/jorge/Documents/data/%d/mix2.wav',i), out2(1:S), FS);
+    audiowrite(sprintf('/home/jorge/Documents/data/%d/target.wav', i), target, FS);
+    audiowrite(sprintf('/home/jorge/Documents/data/%d/echo.wav', i), echo, FS);
+    audiowrite(sprintf('/home/jorge/Documents/data/%d/mic1_target.wav', i), y11(1:S), FS);
+    audiowrite(sprintf('/home/jorge/Documents/data/%d/mic2_target.wav', i), y12(1:S), FS);
+    audiowrite(sprintf('/home/jorge/Documents/data/%d/mic1_echo.wav', i), y21(1:S), FS);
+    audiowrite(sprintf('/home/jorge/Documents/data/%d/mic2_echo.wav', i), y22(1:S), FS);
+end
+disp('finished!');
