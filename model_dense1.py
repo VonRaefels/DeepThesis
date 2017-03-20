14b9e2ca8d42191c6601fcbfd62de703a20a9823
VonRaefels <jorgemadridportillo@gmail.com>
2017-03-20 17:31:00 +0100

    training!

--- model_dense1.py
+++ model_dense1.py
@@ -10,32 +10,40 @@ from keras.optimizers import SGD
 
 def load_data():
     print('loading data...')
-    input_mat = np.save('inputs_model1.npy', allow_pickle=True)
-    target_mat= np.save('targets_model1.npy', allow_pickle=True)
+    input_mat = np.load('inputs_dense1.npy', allow_pickle=True)
+    target_mat= np.load('targets_dense1.npy', allow_pickle=True)
     return (input_mat, target_mat)
 
 
 def create_model():
     print('-----creating model-----')
     model = Sequential()
-    model.add(Dense(1024, batch_input_shape=(None, 154), activation='relu'))
+    model.add(Dense(1024, batch_input_shape=(None, 308), activation='relu'))
     model.add(Dense(1024, activation='relu'))
     model.add(Dense(1024, activation='relu'))
-    model.add(Dense(154, activation='sigmoid'))
+    model.add(Dense(64, activation='sigmoid'))
 
 
     sgd = SGD(lr=0.003, decay=0.0)
     model.compile(loss='mean_squared_error',
                   optimizer='sgd',
                   metrics=['accuracy'])
+    return model
 
-def train_and_evaluate_model():
+def train_and_evaluate_model(X_train, Y_train, model):
     print('training and evaluation')
-    model.fit(X_train, Y_train, batch_size=199, nb_epoch=2000, verbose=1)
+    model.fit(X_train, Y_train, batch_size=199, nb_epoch=500, verbose=1)
 
 
 if __name__ == "__main__":
     print('Starting...')
-    data, targets = load_data()
+    X_train, Y_train = load_data()
+    X_train = X_train.T
+    Y_train = Y_train.T
+
+    print(X_train.shape)
+    print(Y_train.shape)
+    model = create_model()
+    train_and_evaluate_model(X_train, Y_train, model)
     #n_folds = 1
     #skf = StratifiedKFold(labels, n_folds=n_folds, shuffle=True)
